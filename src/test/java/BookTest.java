ebf4c1cad151ae1eb8baf0b683c3d556a24fca03
Jonny Reading <jonnyreading@gmail.com>
2020-09-22 22:47:23 +0100

    book has a title

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+    private Book book;
+
+    @Before
+    public void before() {
+        book = new Book("The Book Thief", "Markus Zusack", "Novel");
+    }
+
+    @Test
+    public void bookHasATitle() {
+        assertEquals("The Book Thief", book.getTitle());
+    }
+
 }
