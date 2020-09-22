31a577d7557e2b93106e1dad9bd421bfaa757a2d
Jonny Reading <jonnyreading@gmail.com>
2020-09-22 22:53:23 +0100

    library has an empty list of books

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> booksList;
+
+    public Library() {
+        this.booksList = new ArrayList<>();
+    }
+
+    public int numberOfBooks() {
+        return this.booksList.size();
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,3 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before() {
+        library = new Library();
+        book = new Book("The Book Thief", "Markus Zusak", "Novel");
+    }
+
+    @Test
+    public void libraryHasACollectionOfBooks() {
+        assertEquals(0, library.numberOfBooks());
+    }
 
 }
